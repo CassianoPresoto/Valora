0f8e5c486c0c5ed33d9aa62517a6b52f286ed81b
Cassiano Carlos Presoto <cassiano.presoto@dynamox.net>
2025-08-27 19:49:22 -0300

    Create adaptive layout

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -1,48 +1,77 @@
 package org.example.project
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Icon
-import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.stringResource
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.example.project.core.navigation.BottomTab
+import org.example.project.features.account.presentation.AccountScreen
+import org.example.project.features.activities.presentation.ActivitiesScreen
+import org.example.project.features.add.presentation.AddScreen
 import org.example.project.features.friends.presentation.FriendsScreen
 import org.example.project.features.groups.presentation.GroupsScreen
-import org.example.project.features.add.presentation.AddScreen
-import org.example.project.features.activities.presentation.ActivitiesScreen
-import org.example.project.features.account.presentation.AccountScreen
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         var selectedTab by rememberSaveable { mutableStateOf(BottomTab.Friends) }
-
-        Scaffold(
-            bottomBar = {
-                NavigationBar {
-                    BottomTab.entries.forEach { tab ->
-                        NavigationBarItem(
-                            selected = selectedTab == tab,
-                            onClick = { selectedTab = tab },
-                            icon = { Icon(tab.icon, contentDescription = null) },
-                            label = { 
-                                tab.labelRes?.let { labelRes ->
-                                    Text(stringResource(labelRes))
-                                }
+        BoxWithConstraints {
+            val useNavigationRail = maxWidth >= 600.dp
+            if (useNavigationRail) {
+                Row(modifier = Modifier.fillMaxSize()) {
+                    NavigationRail(
+                        modifier = Modifier.fillMaxHeight()
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxHeight(),
+                            verticalArrangement = Arrangement.SpaceEvenly
+                        ) {
+                            BottomTab.entries.forEach { tab ->
+                                NavigationRailItem(
+                                    selected = selectedTab == tab,
+                                    onClick = { selectedTab = tab },
+                                    icon = { Icon(tab.icon, contentDescription = null) },
+                                    label = {
+                                        tab.labelRes?.let { labelRes ->
+                                            Text(stringResource(labelRes))
+                                        }
+                                    }
+                                )
                             }
-                        )
+                        }
                     }
+                    AppContent(selectedTab, PaddingValues(0.dp))
+                }
+            } else {
+                Scaffold(
+                    bottomBar = {
+                        NavigationBar {
+                            BottomTab.entries.forEach { tab ->
+                                NavigationBarItem(
+                                    selected = selectedTab == tab,
+                                    onClick = { selectedTab = tab },
+                                    icon = { Icon(tab.icon, contentDescription = null) },
+                                    label = {
+                                        tab.labelRes?.let { labelRes ->
+                                            Text(stringResource(labelRes))
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    }
+                ) { innerPadding ->
+                    AppContent(selectedTab, innerPadding)
                 }
             }
-        ) { innerPadding ->
-            AppContent(selectedTab, innerPadding)
         }
     }
 }
