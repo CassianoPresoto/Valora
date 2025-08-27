52a010f0b8288f05eeb790686a8952639969c58f
Cassiano Carlos Presoto <cassiano.presoto@dynamox.net>
2025-08-27 20:05:32 -0300

    Update BottomTab.kt
    adjust icon

--- composeApp/src/commonMain/kotlin/org/example/project/core/navigation/BottomTab.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/core/navigation/BottomTab.kt
@@ -1,20 +1,20 @@
 package org.example.project.core.navigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddBox
+import androidx.compose.material.icons.filled.AreaChart
 import androidx.compose.material.icons.filled.Group
-import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.People
 import androidx.compose.ui.graphics.vector.ImageVector
 import org.jetbrains.compose.resources.StringResource
 import valora.composeapp.generated.resources.Res
 import valora.composeapp.generated.resources.*
 
 enum class BottomTab(val labelRes: StringResource?, val icon: ImageVector) {
-    Friends(Res.string.tab_friends, Icons.Default.People),
+    Friends(Res.string.tab_friends, Icons.Default.Person),
     Groups(Res.string.tab_groups, Icons.Default.Group),
-    Add(null, Icons.Default.Add),
-    Activities(Res.string.tab_activities, Icons.Default.List),
-    Account(Res.string.tab_account, Icons.Default.Person)
+    Add(null, Icons.Default.AddBox),
+    Activities(Res.string.tab_activities, Icons.Default.AreaChart),
+    Account(Res.string.tab_account, Icons.Default.AccountCircle)
 }
